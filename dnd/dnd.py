787a53edcd21b506eb3d5b496517b081a658c4ae
JJW <jason.wright.2007@gmail.com>
2017-07-11 16:57:37 -0500

    invoke without command = false

--- dnd/dnd.py
+++ dnd/dnd.py
@@ -9,7 +9,7 @@ class DND:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(pass_context=True, invoke_without_command=True)
+    @commands.group(pass_context=True, invoke_without_command=False)
     async def dnd(self, ctx, *, hargs=''):
         if ctx.invoked_subcommand is None:
             await self.bot.say('DEBUG: hargs = '+ hargs)
