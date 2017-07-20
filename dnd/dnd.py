d4d0cdfcedf75df6e31ed4e31beb5958a40b99b5
JJW <jason.wright.2007@gmail.com>
2017-07-19 19:33:12 -0500

    dnd

--- dnd/dnd.py
+++ dnd/dnd.py
@@ -251,28 +251,28 @@ class DND:
             messages.append(said)
             keys = json_file.keys()
 
-            for key in keys:
-                if key not in {'_id','index','name','desc','actions','legendary_actions'}:
-                    key2 = key.replace('_',' ').title()
-                    if isinstance(json_file[key],list):
-                        try:
-                            em.add_field(name=key2,value='\n'.join(j['name'] for j in json_file[key]))
-                        except:
-                            em.add_field(name=key2,value='\n'.join(j for j in json_file[key]))
-                    elif isinstance(json_file[key],tuple):
-                        try:
-                            em.add_field(name=key2,value='\n'.join(j['name'] for j in json_file[key]))
-                        except:
-                            em.add_field(name=key2,value='\n'.join(j for j in json_file[key]))
-                    elif isinstance(json_file[key],dict):
-                        em.add_field(name=key2,value=json_file[key]['name'])
-                    elif isinstance(json_file[key],str):
-                        em.add_field(name=key2,value=json_file[key])
-                    elif isinstance(json_file[key],int):
-                        em.add_field(name=key2,value=json_file[key])
-                    else:
-                        em.add_field(name=key2,value='something else detected')
-            embeds.append(em)
+            # for key in keys:
+            #     if key not in {'_id','index','name','desc','actions','legendary_actions'}:
+            #         key2 = key.replace('_',' ').title()
+            #         if isinstance(json_file[key],list):
+            #             try:
+            #                 em.add_field(name=key2,value='\n'.join(j['name'] for j in json_file[key]))
+            #             except:
+            #                 em.add_field(name=key2,value='\n'.join(j for j in json_file[key]))
+            #         elif isinstance(json_file[key],tuple):
+            #             try:
+            #                 em.add_field(name=key2,value='\n'.join(j['name'] for j in json_file[key]))
+            #             except:
+            #                 em.add_field(name=key2,value='\n'.join(j for j in json_file[key]))
+            #         elif isinstance(json_file[key],dict):
+            #             em.add_field(name=key2,value=json_file[key]['name'])
+            #         elif isinstance(json_file[key],str):
+            #             em.add_field(name=key2,value=json_file[key])
+            #         elif isinstance(json_file[key],int):
+            #             em.add_field(name=key2,value=json_file[key])
+            #         else:
+            #             em.add_field(name=key2,value='something else detected')
+            # embeds.append(em)
             # for key in ('desc', 'actions','legendary_actions'):
             #     if key in keys:
             #         desc_pages = chat.pagify('\n'.join(json_file[key]), delims=['\n\n'], escape=True, shorten_by=8, page_length=500)
@@ -284,9 +284,9 @@ class DND:
             #             em = discord.Embed(color=COLORS[category],title='',description=page))
             #             embeds.append(em)
 
-            for em in embeds:
-                said = await self.bot.say(embed=em)
-                messages.append(said)
+            # for em in embeds:
+            #     said = await self.bot.say(embed=em)
+            #     messages.append(said)
             last = len(messages)-1
             print(last)
             await self.bot.add_reaction(messages[last], "❌")
